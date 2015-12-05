45c67ec1baa51b71515c19b52ce924506fca378d
Stefan Neidig <stefan@rpdev.net>
2015-12-05 21:53:32 +0100

    Implement basic transition mechanism

--- app/src/main/java/com/dasheck/socialcard/MainActivity.java
+++ app/src/main/java/com/dasheck/socialcard/MainActivity.java
@@ -1,13 +1,91 @@
 package com.dasheck.socialcard;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.dasheck.socialcard.fragments.test;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTouch;
+
+public class MainActivity extends AppCompatActivity implements test.LayoutRevealListener {
+
+    Pair<Integer, Integer> currentTouchPosition;
+
+    Fragment currentFrament;
+    Fragment previousFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnTouch(R.id.clickContainer) public boolean onButtonTouched(MotionEvent motionEvent) {
+        Log.d("TAG", "Touched");
+
+        if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            currentTouchPosition = new Pair<>((int) motionEvent.getX(), (int) motionEvent.getY());
+        }
+
+        return false;
+    }
+
+    @OnClick(R.id.button) public void onFirstClicked(View view) {
+        Log.d("TAG", "Button: Touch position: " + currentTouchPosition.first + ", " + currentTouchPosition.second);
+
+        test fragment = test.newInstance(currentTouchPosition.first, currentTouchPosition.second);
+        addFragment(fragment, "Button");
+    }
+
+    @OnClick(R.id.button2) public void onSecondClicked(View view) {
+        Log.d("TAG", "Button2: Touch position: " + currentTouchPosition.first + ", " + currentTouchPosition.second);
+
+        test fragment = test.newInstance(currentTouchPosition.first, currentTouchPosition.second);
+        addFragment(fragment, "Button2");
+    }
+
+    private void addFragment(test fragment, String tag) {
+        if(currentFrament == null || !tag.equals(currentFrament.getTag())) {
+            getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, fragment, tag).commit();
+
+            previousFragment = currentFrament;
+            currentFrament = fragment;
+
+            fragment.setRevealListener(this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void reavealAnimationEnd(Fragment fragment) {
+        Log.d("TAG", "Current:" + currentFrament.getTag());
+        Log.d("TAG", "COUNT:" + getSupportFragmentManager().getBackStackEntryCount());
+
+        if(previousFragment != null) {
+            Log.d("TAG", "Previous:" + previousFragment.getTag());
+            getSupportFragmentManager().beginTransaction().remove(previousFragment).commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
---
+++ app/src/main/java/com/dasheck/socialcard/fragments/test.java
@@ -0,0 +1,152 @@
+package com.dasheck.socialcard.fragments;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.ViewUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
+
+import com.dasheck.socialcard.R;
+
+import java.util.Random;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.codetail.animation.SupportAnimator;
+import io.codetail.animation.ViewAnimationUtils;
+
+public class test extends Fragment implements SupportAnimator.AnimatorListener {
+
+    private static final String BUNDLE_CX_KEY = "cxKey";
+    private static final String BUNDLE_CY_KEY = "cyKey";
+
+    private boolean revealed = false;
+    private int cx;
+    private int cy;
+
+    @Bind(R.id.container)
+    View container;
+    @Bind(R.id.first)
+    View first;
+    @Bind(R.id.second)
+    View second;
+
+    private LayoutRevealListener revealListener;
+
+    public void setRevealListener(LayoutRevealListener revealListener) {
+        this.revealListener = revealListener;
+    }
+
+    public static test newInstance(int x, int y) {
+        test fragment = new test();
+
+        Bundle args = new Bundle();
+        args.putInt(BUNDLE_CX_KEY, x);
+        args.putInt(BUNDLE_CY_KEY, y);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_test, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        Bundle args = getArguments();
+        cx = args.getInt(BUNDLE_CX_KEY);
+        cy = args.getInt(BUNDLE_CY_KEY);
+
+        Log.d("TAG", "Created my view: " + cx + ", " + cy);
+
+        first.setBackgroundColor(randomColor());
+        second.setBackgroundColor(randomColor());
+
+        first.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                reveal();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onAnimationStart() {
+
+    }
+
+    @Override
+    public void onAnimationEnd() {
+        if (revealListener != null) {
+            revealListener.reavealAnimationEnd(this);
+        }
+    }
+
+    @Override
+    public void onAnimationCancel() {
+
+    }
+
+    @Override
+    public void onAnimationRepeat() {
+
+    }
+
+    public interface LayoutRevealListener {
+
+        void reavealAnimationEnd(Fragment fragment);
+    }
+
+    void reveal() {
+        if (!revealed) {
+            revealed = true;
+
+            View view = container;
+
+            // get the final radius for the clipping circle
+            int dx = Math.max(cx, view.getWidth() - cx);
+            int dy = Math.max(cy, view.getHeight() - cy);
+            float finalRadius = (float) Math.hypot(dx, dy);
+
+            SupportAnimator animator =
+                    ViewAnimationUtils.createCircularReveal(view, cx, cy, 0, finalRadius);
+            animator.setInterpolator(new AccelerateDecelerateInterpolator());
+            animator.setDuration(500);
+            animator.start();
+
+            animator.addListener(this);
+        }
+    }
+
+    static int randomColor() {
+        Random random = new Random();
+        return Color.HSVToColor(new float[]{
+                random.nextInt(360),
+                0.70f,
+                0.70f
+        });
+    }
+}
