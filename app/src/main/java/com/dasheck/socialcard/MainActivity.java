9240e9eaa0ba099e3a5b4124e54cdfacf5721613
Stefan Neidig <stefan@rpdev.net>
2015-12-06 09:11:09 +0100

    Reformat code and remove debug output

--- app/src/main/java/com/dasheck/socialcard/MainActivity.java
+++ app/src/main/java/com/dasheck/socialcard/MainActivity.java
@@ -14,75 +14,70 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnTouch;
 
-public class MainActivity extends AppCompatActivity implements SocialCardFragment.LayoutRevealListener {
+public class MainActivity extends AppCompatActivity
+    implements SocialCardFragment.LayoutRevealListener {
 
-    Pair<Integer, Integer> currentTouchPosition;
+  Pair<Integer, Integer> currentTouchPosition;
 
-    Fragment currentFrament;
-    Fragment previousFragment;
+  Fragment currentFrament;
+  Fragment previousFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+  }
+
+  @OnTouch(R.id.clickContainer) public boolean onButtonTouched(MotionEvent motionEvent) {
+    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+      currentTouchPosition = new Pair<>((int) motionEvent.getX(), (int) motionEvent.getY());
     }
 
-    @OnTouch(R.id.clickContainer) public boolean onButtonTouched(MotionEvent motionEvent) {
-        Log.d("TAG", "Touched");
+    return false;
+  }
 
-        if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-            currentTouchPosition = new Pair<>((int) motionEvent.getX(), (int) motionEvent.getY());
-        }
+  @OnClick(R.id.button) public void onFirstClicked(View view) {
+    SocialCardFragment fragment =
+        SocialCardFragment.newInstance(currentTouchPosition.first, currentTouchPosition.second);
+    addFragment(fragment, "Button");
+  }
 
-        return false;
+  @OnClick(R.id.button2) public void onSecondClicked(View view) {
+    SocialCardFragment fragment =
+        SocialCardFragment.newInstance(currentTouchPosition.first, currentTouchPosition.second);
+    addFragment(fragment, "Button2");
+  }
+
+  private void addFragment(SocialCardFragment fragment, String tag) {
+    if (currentFrament == null || !tag.equals(currentFrament.getTag())) {
+      getSupportFragmentManager().beginTransaction()
+          .add(R.id.fragmentContainer, fragment, tag)
+          .commit();
+
+      previousFragment = currentFrament;
+      currentFrament = fragment;
+
+      fragment.setRevealListener(this);
     }
+  }
 
-    @OnClick(R.id.button) public void onFirstClicked(View view) {
-        Log.d("TAG", "Button: Touch position: " + currentTouchPosition.first + ", " + currentTouchPosition.second);
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    ButterKnife.unbind(this);
+  }
 
-        SocialCardFragment fragment = SocialCardFragment.newInstance(currentTouchPosition.first, currentTouchPosition.second);
-        addFragment(fragment, "Button");
+  @Override
+  public void reavealAnimationEnd(Fragment fragment) {
+    if (previousFragment != null) {
+      getSupportFragmentManager().beginTransaction().remove(previousFragment).commit();
     }
+  }
 
-    @OnClick(R.id.button2) public void onSecondClicked(View view) {
-        Log.d("TAG", "Button2: Touch position: " + currentTouchPosition.first + ", " + currentTouchPosition.second);
-
-        SocialCardFragment fragment = SocialCardFragment.newInstance(currentTouchPosition.first, currentTouchPosition.second);
-        addFragment(fragment, "Button2");
-    }
-
-    private void addFragment(SocialCardFragment fragment, String tag) {
-        if(currentFrament == null || !tag.equals(currentFrament.getTag())) {
-            getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, fragment, tag).commit();
-
-            previousFragment = currentFrament;
-            currentFrament = fragment;
-
-            fragment.setRevealListener(this);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        ButterKnife.unbind(this);
-    }
-
-    @Override
-    public void reavealAnimationEnd(Fragment fragment) {
-        Log.d("TAG", "Current:" + currentFrament.getTag());
-        Log.d("TAG", "COUNT:" + getSupportFragmentManager().getBackStackEntryCount());
-
-        if(previousFragment != null) {
-            Log.d("TAG", "Previous:" + previousFragment.getTag());
-            getSupportFragmentManager().beginTransaction().remove(previousFragment).commit();
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-    }
+  @Override
+  public void onBackPressed() {
+    super.onBackPressed();
+    finish();
+  }
 }
